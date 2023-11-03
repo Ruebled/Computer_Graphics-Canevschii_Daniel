74a422c4912f3279ba8d16b426b0f8c23f4422aa
Ruebled <dan.canevschii@gmail.com>
2023-11-03 21:13:07 +0200

    Adding Randomizer Class

---
+++ Randomizer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+
+namespace Canevschii
+{
+    class Randomizer
+    {
+        private Random r;
+        public Randomizer()
+        {
+            r = new Random();
+        }
+
+        public int GetRandomOffsetPositive(int maxval)
+        {
+            int getInteger = r.Next (0, maxval);
+            
+            return getInteger;
+        }
+
+        public int GetRandomOffsetRanged(int maxval)
+        {
+            int getInteger = r.Next(-1*maxval, maxval);
+            
+            return getInteger;
+        }
+
+        public Color GetRandomColor()
+        {
+            int genR = r.Next(0, 255);
+            int genG = r.Next(0, 255);
+            int genB = r.Next(0, 255);
+            Color col = Color.FromArgb(genR, genG, genB);
+
+            return col;
+        }
+    }
+}
